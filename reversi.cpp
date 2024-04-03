44bf177b67f036cb5b7a5beaf013430ec81b5ac2
Daniel Švehla <svehlda2@fel.cvut.cz>
2024-04-04 01:41:31 +0200

    vectorization comments

--- reversi.cpp
+++ reversi.cpp
@@ -24,28 +24,32 @@ int Reversi::heuristics(Board *state, int moves_delta) {
         score -= ((state->black_bitmap >> i) & 1) * heuristics_map[i];
     }
 #else
-    uint64_t white_bitmaps1[4];
+    // we cannot fit everything whole board into 256bit vector,
+    // we split the board into halves
+    uint64_t white_bitmaps1[4]; // first set of vectors, right half of the board
     uint64_t black_bitmaps1[4];
-    white_bitmaps1[0] = state->white_bitmap;
+    // shift column we want to proccess into the rightest position (column 7)
+    white_bitmaps1[0] = state->white_bitmap; // column 7
     black_bitmaps1[0] = state->black_bitmap;
-    white_bitmaps1[1] = (state->white_bitmap >> 1);
+    white_bitmaps1[1] = (state->white_bitmap >> 1); // column 6
     black_bitmaps1[1] = (state->black_bitmap >> 1);
-    white_bitmaps1[2] = (state->white_bitmap >> 2);
+    white_bitmaps1[2] = (state->white_bitmap >> 2); // column 5
     black_bitmaps1[2] = (state->black_bitmap >> 2);
-    white_bitmaps1[3] = (state->white_bitmap >> 3);
+    white_bitmaps1[3] = (state->white_bitmap >> 3); // column 4
     black_bitmaps1[3] = (state->black_bitmap >> 3);
 
-    uint64_t white_bitmaps2[4];
+    uint64_t white_bitmaps2[4]; // second set of vectors, left half of the board
     uint64_t black_bitmaps2[4];
-    white_bitmaps2[0] = (state->white_bitmap >> 4);
+    white_bitmaps2[0] = (state->white_bitmap >> 4); // column 3
     black_bitmaps2[0] = (state->black_bitmap >> 4);
-    white_bitmaps2[1] = (state->white_bitmap >> 5);
+    white_bitmaps2[1] = (state->white_bitmap >> 5); // column 2
     black_bitmaps2[1] = (state->black_bitmap >> 5);
-    white_bitmaps2[2] = (state->white_bitmap >> 6);
+    white_bitmaps2[2] = (state->white_bitmap >> 6); // column 1
     black_bitmaps2[2] = (state->black_bitmap >> 6);
-    white_bitmaps2[3] = (state->white_bitmap >> 7);
+    white_bitmaps2[3] = (state->white_bitmap >> 7); // column 0
     black_bitmaps2[3] = (state->black_bitmap >> 7);
 
+    // load heuristic matrix values into corresponding positions
     int64_t heur_map1[4];
     heur_map1[0] = 0x64f10a05050af164;
     heur_map1[1] = 0xf1e2fefefefee2f1;
@@ -58,6 +62,7 @@ int Reversi::heuristics(Board *state, int moves_delta) {
     heur_map2[2] = 0xf1e2fefefefee2f1;
     heur_map2[3] = 0x64f10a05050af164;
 
+    // load data into vector registeres
     __m256i heur_vec1 = _mm256_loadu_si256((__m256i *) heur_map1);
     __m256i heur_vec2 = _mm256_loadu_si256((__m256i *) heur_map2);
     __m256i white_vec1 = _mm256_loadu_si256((__m256i *) white_bitmaps1);
@@ -65,25 +70,36 @@ int Reversi::heuristics(Board *state, int moves_delta) {
     __m256i black_vec1 = _mm256_loadu_si256((__m256i *) black_bitmaps1);
     __m256i black_vec2 = _mm256_loadu_si256((__m256i *) black_bitmaps2);
 
+    // mask out everything except the right column 
     __m256i mask_vec = _mm256_set1_epi64x(0x0101010101010101);
     white_vec1 = _mm256_and_si256(white_vec1, mask_vec);
     white_vec2 = _mm256_and_si256(white_vec2, mask_vec);
     black_vec1 = _mm256_and_si256(black_vec1, mask_vec);
     black_vec2 = _mm256_and_si256(black_vec2, mask_vec);
 
+    // at this point white_vec1/white_vec2/black_vec1/black_ve2
+    // are either 0b00000000 or 0b00000001, which is used in multiplication
+    // with heuristic values to get the score efficiently
+
+    // multiply board vectors with heuristics vectors and partially add the results together
+    // multiply two corresponding 8bit signed integers
     __m256i result_white1 = _mm256_maddubs_epi16(white_vec1, heur_vec1);
     __m256i result_white2 = _mm256_maddubs_epi16(white_vec2, heur_vec2);
     __m256i result_black1 = _mm256_maddubs_epi16(black_vec1, heur_vec1);
     __m256i result_black2 = _mm256_maddubs_epi16(black_vec2, heur_vec2);
 
+    // join the results of the two halves
     __m256i result_white_joined = _mm256_add_epi16(result_white1, result_white2);
     __m256i result_black_joined = _mm256_add_epi16(result_black1, result_black2);
 
+    // subtract black score from white score
     __m256i result_vec = _mm256_sub_epi16(result_white_joined, result_black_joined);
 
+    // create data_out array
     int16_t int_result[16];
-    _mm256_storeu_si256((__m256i *) int_result, result_vec);
+    _mm256_storeu_si256((__m256i *) int_result, result_vec); // extract vector result into the array
 
+    // sum all 16bit parts
     for (int i = 0; i < 16; ++i) {
         score += static_cast<int>(int_result[i]);
     }
