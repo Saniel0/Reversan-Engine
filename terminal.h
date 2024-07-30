6e18fbd9351f91d8cd318264a245cbe3204ed18a
Daniel Svehla <daniel.svehla@outlook.com>
2024-07-30 23:37:47 +0200

    added one font

--- terminal.h
+++ terminal.h
@@ -5,6 +5,7 @@
 
 void print_title_blur();
 void print_title_3d();
+void print_title_tile();
 void delete_lines(int count);
 void print_boards(Board *last_state, Board *current_state, bool current_color);
 
