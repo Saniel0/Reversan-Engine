70d87fe3bbb4e73aa003ebdb149b4e3882641731
Daniel Svehla <daniel.svehla@outlook.com>
2024-07-30 20:12:30 +0200

    enhanced terminal output

--- main.cpp
+++ main.cpp
@@ -1,40 +1,7 @@
 #include <iostream>
 #include <random>
 #include "reversi.h"
-
-void print_title() {
-    std::cout << "          _____                    _____                    _____                    _____                    _____                    _____                    _____          \n";
-    std::cout << "         /\\    \\                  /\\    \\                  /\\    \\                  /\\    \\                  /\\    \\                  /\\    \\                  /\\    \\         \n";
-    std::cout << "        /::\\    \\                /::\\    \\                /::\\____\\                /::\\    \\                /::\\    \\                /::\\    \\                /::\\    \\        \n";
-    std::cout << "       /::::\\    \\              /::::\\    \\              /:::/    /               /::::\\    \\              /::::\\    \\              /::::\\    \\               \\:::\\    \\       \n";
-    std::cout << "      /::::::\\    \\            /::::::\\    \\            /:::/    /               /::::::\\    \\            /::::::\\    \\            /::::::\\    \\               \\:::\\    \\      \n";
-    std::cout << "     /:::/\\:::\\    \\          /:::/\\:::\\    \\          /:::/    /               /:::/\\:::\\    \\          /:::/\\:::\\    \\          /:::/\\:::\\    \\               \\:::\\    \\     \n";
-    std::cout << "    /:::/__\\:::\\    \\        /:::/__\\:::\\    \\        /:::/____/               /:::/__\\:::\\    \\        /:::/__\\:::\\    \\        /:::/__\\:::\\    \\               \\:::\\    \\    \n";
-    std::cout << "   /::::\\   \\:::\\    \\      /::::\\   \\:::\\    \\       |::|    |               /::::\\   \\:::\\    \\      /::::\\   \\:::\\    \\       \\:::\\   \\:::\\    \\              /::::\\    \\   \n";
-    std::cout << "  /::::::\\   \\:::\\    \\    /::::::\\   \\:::\\    \\      |::|    |     _____    /::::::\\   \\:::\\    \\    /::::::\\   \\:::\\    \\    ___\\:::\\   \\:::\\    \\    ____    /::::::\\    \\  \n";
-    std::cout << " /:::/\\:::\\   \\:::\\____\\  /:::/\\:::\\   \\:::\\    \\     |::|    |    /\\    \\  /:::/\\:::\\   \\:::\\    \\  /:::/\\:::\\   \\:::\\____\\  /\\   \\:::\\   \\:::\\    \\  /\\   \\  /:::/\\:::\\    \\ \n";
-    std::cout << "/:::/  \\:::\\   \\:::|    |/:::/__\\:::\\   \\:::\\____\\    |::|    |   /::\\____\\/:::/__\\:::\\   \\:::\\____\\/:::/  \\:::\\   \\:::|    |/::\\   \\:::\\   \\:::\\____\\/::\\   \\/:::/  \\:::\\____\\\n";
-    std::cout << "\\::/   |::::\\  /:::|____|\\:::\\   \\:::\\   \\::/    /    |::|    |  /:::/    /\\:::\\   \\:::\\   \\::/    /\\::/   |::::\\  /:::|____|\\:::\\   \\:::\\   \\::/    /\\:::\\  /:::/    \\::/    /\n";
-    std::cout << " \\/____|:::::\\/:::/    /  \\:::\\   \\:::\\   \\/____/     |::|    | /:::/    /  \\:::\\   \\:::\\   \\/____/  \\/____|:::::\\/:::/    /  \\:::\\   \\:::\\   \\/____/  \\:::\\/:::/    / \\/____/ \n";
-    std::cout << "       |:::::::::/    /    \\:::\\   \\:::\\    \\         |::|____|/:::/    /    \\:::\\   \\:::\\    \\            |:::::::::/    /    \\:::\\   \\:::\\    \\       \\::::::/    /          \n";
-    std::cout << "       |::|\\::::/    /      \\:::\\   \\:::\\____\\        |:::::::::::/    /      \\:::\\   \\:::\\____\\           |::|\\::::/    /      \\:::\\   \\:::\\____\\       \\::::/____/           \n";
-    std::cout << "       |::| \\::/____/        \\:::\\   \\::/    /        \\::::::::::/____/        \\:::\\   \\::/    /           |::| \\::/____/        \\:::\\  /:::/    /        \\:::\\    \\           \n";
-    std::cout << "       |::|  ~|               \\:::\\   \\/____/          ~~~~~~~~~~               \\:::\\   \\/____/            |::|  ~|               \\:::\\/:::/    /          \\:::\\    \\          \n";
-    std::cout << "       |::|   |                \\:::\\    \\                                        \\:::\\    \\                |::|   |                \\::::::/    /            \\:::\\    \\         \n";
-    std::cout << "       \\::|   |                 \\:::\\____\\                                        \\:::\\____\\               \\::|   |                 \\::::/    /              \\:::\\____\\        \n";
-    std::cout << "        \\:|   |                  \\::/    /                                         \\::/    /                \\:|   |                  \\::/    /                \\::/    /        \n";
-    std::cout << "         \\|___|                   \\/____/                                           \\/____/                  \\|___|                   \\/____/                  \\/____/         \n";
-}
-
-void print_title_alt() {
-    std::cout << "░▒▓███████▓▒░░▒▓████████▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓████████▓▒░▒▓███████▓▒░ ░▒▓███████▓▒░▒▓█▓▒░\n";
-    std::cout << "░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░\n";
-    std::cout << "░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░       ░▒▓█▓▒▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░\n";
-    std::cout << "░▒▓███████▓▒░░▒▓██████▓▒░  ░▒▓█▓▒▒▓█▓▒░░▒▓██████▓▒░ ░▒▓███████▓▒░ ░▒▓██████▓▒░░▒▓█▓▒░\n";
-    std::cout << "░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        ░▒▓█▓▓█▓▒░ ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░\n";
-    std::cout << "░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        ░▒▓█▓▓█▓▒░ ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░\n";
-    std::cout << "░▒▓█▓▒░░▒▓█▓▒░▒▓████████▓▒░  ░▒▓██▓▒░  ░▒▓████████▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓███████▓▒░░▒▓█▓▒░\n";
-}
+#include "terminal.h"
 
 void run_bench() {
     Board *init_board = new Board();
@@ -63,35 +30,35 @@ void run_bot_vs_bot_bench() {
 }
 
 void play_against_bot() {
-    Board *init_board = new Board();
+    Board *last_board = new Board();
+    Board *current_board = new Board();
     Reversi *engine = new Reversi();
     
-    std::cout << "Setting up board.\n";
-    init_board->load_start_state();
-    init_board->print_board();
-
+    current_board->load_start_state();
     int white_score = 0;
     int black_score = 0;
     bool at_turn = false;
     bool last_moved = true;
 
+    print_boards(last_board, current_board, at_turn);
+    std::cout << "\n";
     while (true) {
-        uint64_t possible_moves = init_board->find_moves(at_turn);
+        uint64_t possible_moves = current_board->find_moves(at_turn);
         if (possible_moves == 0) {
             if (!last_moved) {
                 std::cout << "Game ended\n";
-                white_score = __builtin_popcountll(init_board->white_bitmap);
-                black_score = __builtin_popcountll(init_board->black_bitmap);
+                white_score = __builtin_popcountll(current_board->white_bitmap);
+                black_score = __builtin_popcountll(current_board->black_bitmap);
                 break;
             }
             last_moved = false;
             at_turn = !at_turn;
-            std::cout << !at_turn << " cannot move\n";
             continue;
         }
         last_moved = true;
         if (at_turn) {
-            init_board->print_board_moves(possible_moves);
+            delete_lines(13);
+            print_boards(last_board, current_board, at_turn);
             uint64_t move;
             while (true) {
                 std::cout << "Select move > ";
@@ -101,22 +68,25 @@ void play_against_bot() {
                 if (move & possible_moves) {
                     break;
                 }
-                std::cout << "Invalid move!" << '\n';
+                delete_lines(1);
+                std::cout << "INVALID - ";
             }
-            init_board->play_move(at_turn, move);
+            last_board->copy_state(current_board);
+            current_board->play_move(at_turn, move);
         }
         else {
             uint64_t move;
-            move = engine->find_best_move(init_board, at_turn, 10);
-            init_board->play_move(at_turn, move);
+            move = engine->find_best_move(current_board, at_turn, 10);
+            last_board->copy_state(current_board);
+            current_board->play_move(at_turn, move);
         }
         at_turn = !at_turn;
-        init_board->print_board();
     }
     std::cout << '\n' << white_score << ' ' << black_score << '\n';
 }
 
 int main() {
-    print_title_alt();
-    run_bot_vs_bot_bench();
+    print_title_blur();
+    play_against_bot();
+    return 0;
 }
